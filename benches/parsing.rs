7439b105e05612c1f8e2b75cdfd633544923df00
Guillaume W. Bres <guillaume.bressaix@gmail.com>
2025-08-17 14:50:17 +0200

    add benchmarks
    Signed-off-by: Guillaume W. Bres <guillaume.bressaix@gmail.com>

---
+++ benches/parsing.rs
@@ -0,0 +1,23 @@
+extern crate criterion;
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use ionex::prelude::IONEX;
+
+fn ionex_parsing(path: &str) {
+    let _ = IONEX::from_gzip_file(path).unwrap();
+}
+
+fn benchmark(c: &mut Criterion) {
+    let mut parsing_grp = c.benchmark_group("parsing");
+
+    parsing_grp.bench_function("IONEX/V1", |b| {
+        b.iter(|| {
+            ionex_parsing("data/IONEX/V1/CKMG0020.22I.gz");
+        })
+    });
+
+    parsing_grp.finish();
+}
+
+criterion_group!(benches, benchmark);
+criterion_main!(benches);
